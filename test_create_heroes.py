d771a6e8d0e24706af1c75be9e40b36a123d92cc
Aude <aude.lekeux@student.unamur.be>
2019-03-16 18:19:52 +0100

    create heroes

--- test_create_heroes.py
+++ test_create_heroes.py
@@ -1,15 +1,36 @@
-def create_heroes(positions, classes):
+def initializaion():
+
+    positions = {(10, 24): 'Wolf', (35, 20): 'Lion'}
+
+    classes = {'barbarian': {'energise': [[1, 1, 1], [2, 1, 1], [3, 2, 1], [4, 2, 1]],
+                             'stun': [[1, 1, 1], [2, 2, 2], [3, 3, 1]]},
+               'healer': {'invigorate': [[1, 1, 1], [2, 2, 1], [3, 3, 1], [4, 4, 1]],
+                          'immunise': [[1, 0, 1], [2, 0, 3], [3, 0, 3]]},
+               'mage': {'fulgura': [[1, 3, 1], [2, 3, 1], [3, 4, 1], [4, 4, 1]],
+                        'ovibus': [[1, 1, 3], [2, 2, 3], [3, 2, 3]]},
+               'rogue': {'reach': [[1, 0, 1], [2, 0, 1], [3, 0, 1], [4, 0, 1]],
+                         'brust': [[1, 1, 1], [2, 2, 1], [3, 3, 1]]}}
+
+
+    player1 = create_heroes(classes)
+    # player2 = create_heroes(classes)
+
+    print(player1)
+
+
+def create_heroes(classes):
     """Takes the player's input, splits the information and stores it into a dictionary.
 
     Parameter:
     ----------
     positions: Contains all the coordinates of the board (dict)
+    classes:
+    hero_number:
 
     Returns:
     --------
     positions: Contains all the coordinates of the board and the heroes (dict)
-    player1: Level, number of point, etc. of the heroes of player1 (dict)
-    player2: Level, number of point, etc. of the heroes of player2 (dict)
+    player: Level, number of point, etc. of the heroes of player (dict)
 
     Notes:
     ------
@@ -21,44 +42,37 @@ def create_heroes(positions, classes):
     implementation: Aude Lekeux (v.2 15/03/19)
     """
 
-    player_input = input(str('Enter your four heroes and their type[name:type]: '))
-
-    # conditions syntax + type: healer mage rogue barbarian (nothing else possible)
+    # conditions syntax
     # ask again while syntax is invalid
+    player = {}
     invalid_syntax = True
-
-    player_input = player_input.split(" ")
     
-    while invalid_syntax:
+    while invalid_syntax and len(player) < 4:
         invalid_syntax = False
+        player_input = input(str('Enter your four heroes and their type[name:type]: '))
+        player_input = player_input.split(" ")
 
         for sp in player_input:
             name, type = sp.split(":")
 
-            #for letter in name:
-
             if not name.isalpha():
-                print('The name is not appropriated, it should contain only letters! ')
+                print('The name ' + name + ' is not appropriated, it should contain only letters!')
                 invalid_syntax = True
-            else:
-                positions[name] = type
 
-            if type not in classes:
-                print('The type of your hero is unknown! ')
+            elif type not in classes:
+                print('The type ' + type + ' of your hero is unknown!')
+                invalid_syntax = True
 
-    print(positions)
+            elif name not in player:
+                player[name] = type
 
+            elif name in player:
+                print('The name ' + name + ' is already taken!')
+                player = {}
+                invalid_syntax = True
 
-positions = {(10, 24): 'Wolf', (35, 20): 'Lion'}
-classes = {'barbarian': {'energise': [[1, 1, 1], [2, 1, 1], [3, 2, 1], [4, 2, 1]],
-                            'stun': [[1, 1, 1], [2, 2, 2], [3, 3, 1]]},
-              'healer': {'invigorate': [[1, 1, 1], [2, 2, 1], [3, 3, 1], [4, 4, 1]],
-                         'immunise': [[1, 0, 1], [2, 0, 3], [3, 0, 3]]},
-              'mage': {'fulgura': [[1, 3, 1], [2, 3, 1], [3, 4, 1], [4, 4, 1]],
-                       'ovibus': [[1, 1, 3], [2, 2, 3], [3, 2, 3]]},
-              'rogue':{'reach': [[1, 0, 1], [2, 0, 1], [3, 0, 1], [4, 0, 1]],
-                       'brust': [[1, 1, 1], [2, 2, 1], [3, 3, 1]]}}
+    return player
 
-print(create_heroes(positions, classes))
 
 # Baz:barbarian Lee:healer May:mage Rob:rogue
+initializaion()
--- test_level.py
+++ test_level.py
@@ -92,11 +92,11 @@ def level(player, classes):
 
 classes = {'barbarian': {'energise': [[1, 1, 1], [2, 1, 1], [3, 2, 1], [4, 2, 1]],
                             'stun': [[1, 1, 1], [2, 2, 2], [3, 3, 1]]},
-              'healer': {'invigorate': [[1, 1, 1], [2, 2, 1], [3, 3, 1], [4, 4, 1]],
+            'healer': {'invigorate': [[1, 1, 1], [2, 2, 1], [3, 3, 1], [4, 4, 1]],
                          'immunise': [[1, 0, 1], [2, 0, 3], [3, 0, 3]]},
-              'mage': {'fulgura': [[1, 3, 1], [2, 3, 1], [3, 4, 1], [4, 4, 1]],
+            'mage': {'fulgura': [[1, 3, 1], [2, 3, 1], [3, 4, 1], [4, 4, 1]],
                        'ovibus': [[1, 1, 3], [2, 2, 3], [3, 2, 3]]},
-              'rogue':{'reach': [[1, 0, 1], [2, 0, 1], [3, 0, 1], [4, 0, 1]],
+            'rogue':{'reach': [[1, 0, 1], [2, 0, 1], [3, 0, 1], [4, 0, 1]],
                        'brust': [[1, 1, 1], [2, 2, 1], [3, 3, 1]]}}
 
 player1 = {'Baz': {'class': 'barbarian', 'level': 1, 'life_points': 10, 'victory_points': 0, 'damage_points': 2},
