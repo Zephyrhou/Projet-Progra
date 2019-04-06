6803551cf08b55a4190b92417b74293c3294b7f3
Aude <aude.lekeux@student.unamur.be>
2019-04-06 14:48:25 +0200

    move finished (if position is already taken)

--- move.py
+++ move.py
@@ -23,30 +23,33 @@ def move(positions, hero, movement):
     Version:
     --------
     specification: Zephyr Houyoux (v.5 04/04/19)
-    implementation: Zephyr Houyoux (v.3 4/04/19)
+    implementation: Zephyr Houyoux (v.4 06/04/19)
     """
 
     position_hero = positions[hero]
     # Computes the gap between the position of the hero and where he wants to go
     gap = gap_calculator(movement, position_hero)
-    error_message = 'You can\'t move there'
 
-    # If the position the hero wants to go to is the same as where he is
+    # If the hero is already on the position he wants to go on
     if movement[0] == positions[hero][0] and movement[1] == positions[hero][1]:
-        return error_message
+        return 'Your are already in this position'
+    # If the position the hero wants to go on is already taken
+    for key in positions:
+        if positions[key][0] == movement[0] and positions[key][1] == movement[1]:
+            return 'This position is already taken'
     else:
         # If the gap is less than 1.5 he can move
         if gap < 1.5:
             positions[hero] = movement
             return positions
         else:
-            return error_message
+            return 'This position is too far from where you are'
 
 
 positions = {('20', '3'): 'spawn_player_1', ('20', '37'): 'spawn_player_2', 'Baz': ('10', '3'), 'Lee': ('24', '3'),
-             'May': ('14', '6'), 'Rob': ('20', '17'), 'Buf': ('20', '17'), 'Lia': ('19', '7'), 'Mey': ('3', '3'),
+             'May': ('14', '6'), 'Rob': ('20', '17'), 'Buf': ('20', '17'), 'Lia': ('19', '17'), 'Mey': ('3', '3'),
              'Tob': ('2', '37'), ('20', '38'): 'spur', ('20', '39'): 'spur', ('21', '38'): 'spur',
              ('21', '39'): 'spur', ('10', '10'): ['bear', '20', '5', '3', '100'],
              ('10', '20'): ['bear', '20', '5', '3', '100'], ('15', '10'): ['wolf', '10', '3', '2', '50']}
 
-print(move(positions, 'Buf', ('20', '18')))
+print(move(positions, 'Buf', ('19', '17')))
