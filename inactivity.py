2d8697c3c146866163fc6e9a0b98e2e235c5ad94
Aude <aude.lekeux@student.unamur.be>
2019-04-06 14:10:55 +0200

    inactivity finished

--- inactivity.py
+++ inactivity.py
@@ -1,25 +1,62 @@
-def inactivity(positions, inactivity, incativity_time):
+def inactivity(positions, initial_positions, creatures, initial_creatures, inactivity_time):
     """Count the inactivity of the players.
 
     Parameters:
     -----------
-    positions: Contains all the coordinates of the board and the heroes(dict)
-    inactivity: Contains all the coordinates of the board and the heroes of the previous turn(dict)
+    positions: Contains all the coordinates of the board and the heroes (dict)
+    initial_positions: Contains all the coordinates of the board and the heroes before changes (dict)
+    creatures: Has every information of each creature (dict)
+    initial_creatures: Has every information of each creature before changes (dict)
+    inactivity_time: Number of turns no changes has been made (int)
 
     Returns:
     --------
-    inactivity: Contains all the coordinates of the board and the heroes of the previous turn(dict)
-    inactivity_time: Number of inactivity turn(int)
+    inactivity_time: Number of turns where the game is inactive (int)
+    is_game_over: If the game is over or not depending on inactivity_time (bool)
+
+    Notes:
+    ------
+    If no changes has been made in the game (no moves, no attacks, etc.) for 40 turns then the game is over.
 
     Versions:
     ---------
-    specification: Zéphyr Houyoux(v.1 17/03/19)
-    implementation: Zéphyr Houyoux(v.2 17/03/19)
+    specification: Zéphyr Houyoux(v.2 05/04/19)
+    implementation: Zéphyr Houyoux(v.3 05/04/19)
     """
 
-    if positions == inactivity:
-        inactivity_time =+ 1
-    inactivity = positions
+    is_game_over = False
 
-    return inactivity, inactivity_time
+    # If no changes has been made to positions
+    if positions == initial_positions:
+        # If no changes has been made in creatures
+        if len(creatures) == len(initial_creatures):
 
+            # Then inactivity increased by one
+            inactivity_time += 1
+            if inactivity_time >= 40:
+                is_game_over = True
+            else:
+                is_game_over = False
+
+    return inactivity_time, is_game_over
+
+
+initial_positions = {('20', '3'): 'spawn_player_1', ('20', '37'): 'spawn_player_2', 'Baz': ('10', '3'),
+                     'Lee': ('24', '3'), 'May': ('14', '6'), 'Rob': ('20', '17'), 'Buf': ('20', '17'),
+                     'Lia': ('19', '7'), 'Mey': ('3', '3'), 'Tob': ('2', '37'), ('20', '38'): 'spur',
+                     ('20', '39'): 'spur', ('21', '38'): 'spur', ('21', '39'): 'spur',
+                     ('10', '10'): ['bear', '20', '5', '3', '100'], ('10', '20'): ['bear', '20', '5', '3', '100'],
+                     ('15', '10'): ['wolf', '10', '3', '2', '50']}
+
+positions = {('20', '3'): 'spawn_player_1', ('20', '37'): 'spawn_player_2', 'Baz': ('10', '3'),
+             'Lee': ('24', '3'), 'May': ('14', '6'), 'Rob': ('20', '17'), 'Buf': ('20', '17'), 'Lia': ('19', '7'),
+             'Mey': ('3', '3'), 'Tob': ('2', '37'), ('20', '38'): 'spur', ('20', '39'): 'spur', ('21', '38'): 'spur',
+             ('21', '39'): 'spur', ('10', '10'): ['bear', '20', '5', '3', '100'],
+             ('10', '20'): ['bear', '20', '5', '3', '100'], ('15', '10'): ['wolf', '10', '3', '2', '50']}
+
+initial_creatures = ['bear', 'bear', 'wolf']
+creatures = ['bear', 'bear', 'wolf']
+
+inactivity_time = 36
+
+print(inactivity(positions, initial_positions, creatures, initial_creatures, inactivity_time))
